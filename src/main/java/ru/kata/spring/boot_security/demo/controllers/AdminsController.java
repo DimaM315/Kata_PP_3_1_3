b873039dc2ad6ed5b2cfb258fa39103222816ba4
Dmintiy <dima2000malischew2@mail.ru>
2023-03-27 17:28:08 +0500

    1.0

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminsController.java
@@ -0,0 +1,60 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminsController {
+
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+
+    @GetMapping()
+    public String userList(Model model) {
+        model.addAttribute("users", userServiceImpl.allUsers());
+        return "admin-page";
+    }
+
+    @GetMapping("/user-page/{id}")
+    public String selectUser(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userServiceImpl.fineOne(id).get());
+        return "user-page";
+    }
+
+    @GetMapping("/new")
+    public String newUser(Model model) {
+        model.addAttribute("user", new User());
+        return "new-user";
+    }
+
+    @PostMapping("/create")
+    public String addUser(@ModelAttribute("user") User user) {
+        userServiceImpl.saveUser(user);
+        return "redirect:/admin/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateUser(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userServiceImpl.fineOne(id).get());
+        return "update-user";
+    }
+
+    @PatchMapping("/update/{id}")
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+        userServiceImpl.update(id, user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userServiceImpl.deleteUser(id);
+        return "redirect:/admin";
+    }
+
+
+}
